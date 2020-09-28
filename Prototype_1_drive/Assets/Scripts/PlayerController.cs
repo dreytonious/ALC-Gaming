6b559fcbd2a4176867f8aca28e6069021f71a942
Drey <dreyton.w674@stu.nebo.edu>
2020-09-28 09:06:10 -0600

    Updated Challenge 1

--- Prototype_1_drive/Assets/Scripts/PlayerController.cs
+++ Prototype_1_drive/Assets/Scripts/PlayerController.cs
@@ -14,14 +14,15 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+        
     // Moves the vehicle forward amd back based on VerticalInput
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
     // Moves the vehicle left and right based on horizontalInput
         transform.Rotate(Vector3.up,turnSpeed * horizontalInput * Time.deltaTime);
     }
