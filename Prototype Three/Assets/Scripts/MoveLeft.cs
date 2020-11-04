146f77f97be6a6b2d6c3d96745bc08f1412c68f1
Drey <dreyton.w674@stu.nebo.edu>
2020-11-04 09:13:51 -0700

    Prototype 3 scripts

---
+++ Prototype Three/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
