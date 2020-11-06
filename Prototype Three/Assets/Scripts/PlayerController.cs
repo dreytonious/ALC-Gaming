900010690378bb6d8d134944748d42bca17714c7
Drey <dreyton.w674@stu.nebo.edu>
2020-11-06 09:10:01 -0700

    Prototype 3 move background

--- Prototype Three/Assets/Scripts/PlayerController.cs
+++ Prototype Three/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+
     {
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
-
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OncollisionEnter(Collision collision)
     {
-        isOnGround = true;
+   
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) 
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
     }
+  
 }
--- Prototype Three/Assets/Scripts/SpawnManager.cs
+++ Prototype Three/Assets/Scripts/SpawnManager.cs
@@ -6,22 +6,16 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private float startDelay = 2;
-    private float repeatDelay = 2;
 
     // Start is called before the first frame update
     void Start()
-
-    {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
-    }
-
-    void SpawnObstacle()
     {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
-    
-
-  
