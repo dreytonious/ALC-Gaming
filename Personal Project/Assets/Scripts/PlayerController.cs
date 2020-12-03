7325c5a945c1802d4eb592b189c88119a3a482e3
Drey <dreyton.w674@stu.nebo.edu>
2020-12-03 09:10:45 -0700

    personal project side movement constraint

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
+    public float xRange = 9.50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +17,30 @@ public class PlayerController : MonoBehaviour
     {
         Rigidbody rb = GetComponent<Rigidbody>();
 
+        // Moves the player right when key is pressed
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-
+        // Moves the player left when key is pressed
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
+
+        //Places a boundary on the right
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //Places a boundary to the left
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
     }
 
 
-    
+
 }
